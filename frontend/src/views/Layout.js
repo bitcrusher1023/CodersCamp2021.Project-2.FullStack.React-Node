870d0b4a43c82e5a65eab8e45cb7017de5629a53
RichardHan0314 <shinystar0314@gmail.com>
2023-03-29 02:02:03 -0400

    2023/3/29

--- frontend/src/views/Layout.js
+++ frontend/src/views/Layout.js
@@ -6,15 +6,14 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <>
+    <div>
       <TopBar />
       <Navigation />
       <RightSidebar />
       <MainSection>
         <Outlet />
       </MainSection>
-    </>
+    </div>
   );
 };
-
 export default Layout;
